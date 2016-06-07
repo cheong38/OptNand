5112259a1b1f421976ae075f691f24654a999224
woojin,jung <woojin.jung@cmlatte.com>
2016-06-07 20:53:00 +0900

    파일 읽기 구현.

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,73 @@
  */
 public class Main {
     public static void main(String args[]) {
-        System.out.println("Hello, World");
-        System.out.println("Hello, Jongho");
+        final TraceInfo info = new TraceInfo(10);
+
+        TraceReader.read("res/trace-redis.txt", new ITraceProcceable() {
+            @Override
+            public void handleTrace(Trace tr) {
+                // 파일 읽은 한 줄 할 줄이 Trace 클래스로 담겨서 전달됨.
+                info.inputTrace(tr);
+            }
+        });
+
+        System.out.printf("%d, %d, %d / %d", info.getReadChunkCount(), info.getWriteChunkCount(), info.getInstReadChunkCount(), info.getTotalCount());
+    }
+}
+
+class TraceInfo {
+    private int chunkCriteria;
+
+    private int totalCount = 0;
+    private int readChunkCount = 0;
+    private int instReadChunkCount = 0;
+    private int writeChunkCount = 0;
+
+    private int previousAccessType = -1;
+    private int subsequentAccessCount = 0;
+
+    TraceInfo(int chuckCriteria) {
+        if (chuckCriteria < 1) {
+            // default
+            this.chunkCriteria = 10;
+        }
+
+        this.chunkCriteria = chuckCriteria;
+    }
+
+    public void inputTrace(Trace tr) {
+        if (previousAccessType == tr.accessType) {
+            subsequentAccessCount++;
+        } else {
+            if (subsequentAccessCount > chunkCriteria) {
+                if (previousAccessType == Trace.ACCESS_TYPE_DATA_READ) {
+                    readChunkCount++;
+                } else if (previousAccessType == Trace.ACCESS_TYPE_DATA_WRITE || previousAccessType == Trace.ACCESS_TYPE_FILE_WRITE) {
+                    writeChunkCount++;
+                } else {
+                    instReadChunkCount++;
+                }
+            }
+            subsequentAccessCount = 0;
+        }
+
+        previousAccessType = tr.accessType;
+        totalCount++;
+    }
+
+    public int getTotalCount() {
+        return totalCount;
+    }
+
+    public int getReadChunkCount() {
+        return readChunkCount;
+    }
+
+    public int getInstReadChunkCount() {
+        return instReadChunkCount;
+    }
+
+    public int getWriteChunkCount() {
+        return writeChunkCount;
     }
 }
---
+++ src/TraceReader.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.math.BigInteger;
+
+/**
+ * Created by woojin on 2016. 6. 7..
+ */
+public class TraceReader {
+    public static void read(String filename, ITraceProcceable cb) {
+        if (filename.length() < 1) {
+            throw new Error("Filename should be specified");
+        }
+
+        int READ_CHUNK = 100000;
+
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(filename)));
+            String s;
+            int cnt = 1;
+            int chuckCount = 1;
+            while ((s = reader.readLine()) != null) {
+                if (cnt % READ_CHUNK == 0) {
+                    System.out.printf("%d lines read... - %d\n", READ_CHUNK, chuckCount);
+                    cnt = 0;
+                    chuckCount++;
+                }
+
+                String[] traceComponents = s.split(" ");
+                Trace tr = new Trace();
+                tr.accessType = Integer.parseInt(traceComponents[0], 10);
+                tr.address = new BigInteger(getHexStringWithoutPrefix(traceComponents[1]), 16);
+
+                cb.handleTrace(tr);
+
+                cnt ++;
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String getHexStringWithoutPrefix(String hex) {
+        return hex.startsWith("0x") ? hex.substring(2) : hex;
+    }
+}
+
+class Trace {
+    static final int ACCESS_TYPE_DATA_READ = 0;
+    static final int ACCESS_TYPE_DATA_WRITE = 1;
+    static final int ACCESS_TYPE_INSTRUCTION_READ = 2;
+    static final int ACCESS_TYPE_FILE_WRITE = 3;
+
+    int accessType;
+    BigInteger address;
+}
+
+interface ITraceProcceable {
+    void handleTrace(Trace tr);
+}
